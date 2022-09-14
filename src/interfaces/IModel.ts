ac7c6bf73a56075285ac704b62fe609ef5de972e
Kaio G <kaiogreco91@gmail.com>
2022-09-14 17:29:00 -0300

    feat: cria a interface IModel

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,1 +1,7 @@
-// commit inicial
+export interface IModel<T> {
+  create(obj:T):Promise<T>,
+  read():Promise<T[]>,
+  readOne(_id:string):Promise<T | null>,
+  update(_id: string, obj:T):Promise<T | null>,
+  delete(_id: string):Promise<T | null>,
+}
