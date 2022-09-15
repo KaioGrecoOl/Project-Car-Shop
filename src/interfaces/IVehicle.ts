0c3264737b4fb0bd3f8999fa1d352937bb57ddec
Kaio G <kaiogreco91@gmail.com>
2022-09-15 15:08:43 -0300

    feat: cria a interface IVehicle

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+const AutoZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().gte(1900).lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+type IVehicle = z.infer<typeof AutoZodSchema>;
+
+export { AutoZodSchema, IVehicle };
