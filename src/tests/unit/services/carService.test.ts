939b9a8bd55dde9f069eb1a2455659cb56c3e59c
Kaio G <kaiogreco91@gmail.com>
2022-09-18 20:37:44 -0300

    feat: elabora teste da camara carService

--- src/tests/unit/services/carService.test.ts
+++ src/tests/unit/services/carService.test.ts
@@ -6,6 +6,7 @@ import chai from 'chai';
 import CarService from '../../../services/CarService';
 import CarsModels from '../../../models/CarsModel';
 import { carMockWithId } from '../../mocks/carsMocks';
+import { ErrorTypes } from '../../../errors/catalog';
 const { expect } = chai;
 
 describe('Car Service test', () => {
@@ -17,21 +18,43 @@ describe('Car Service test', () => {
     sinon
       .stub(carModel, 'create')
       .resolves(carMockWithId);
+    sinon
+      .stub(carModel, 'readOne')
+       .onCall(0).resolves(carMockWithId)
+       .onCall(1).resolves(null)
+       .onCall(2).resolves(carMockWithId)
   });
 
   after(()=>{
     sinon.restore();
   })
 
-  it('return a object with keys', async () => {
-    const car = await carService.create(carMockWithId);
-    expect(car).to.have.all.keys(['_id',
-    'model',
-    'year',
-    'color',
-    'buyValue',
-    'doorsQty',
-    'seatsQty'])
-  });
+  describe('creating a car', () => {
+    it('return a object with keys', async () => {
+      const car = await carService.create(carMockWithId);
+      expect(car).to.have.all.keys(['_id',
+      'model',
+      'year',
+      'color',
+      'buyValue',
+      'doorsQty',
+      'seatsQty'])
+    });
+  })
 
+  describe('ReadOne car', () => {
+    it('Success', async () => {
+      const car = await carService.readOne(carMockWithId._id);
+      expect(car).to.be.deep.equal(carMockWithId);
+    })
+
+    it('Failure', async () => {
+      try { 
+        await carService.readOne(carMockWithId._id);
+      } catch (error: any) {
+        expect(error.message).to.be.deep.equal(ErrorTypes.EntityNotFound)
+      }
+    })
+  })
+    
 });
